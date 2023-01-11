ebdc00917e752f50693a7d9d08ae9daeba796250
jpalmer37 <john.palmer1288@gmail.com>
2023-01-11 15:58:11 -0800

    made fetch more robust

--- entrez/fetch.py
+++ entrez/fetch.py
@@ -10,6 +10,17 @@ if len(sys.argv) != 3:
 with open(sys.argv[1]) as handle:
     accnos = pd.Series([x.strip('" \n').lstrip('>') for x in handle.readlines()])
 
+def split_groups(iterable, batch_size=5000):
+	num_groups = len(accnos) // batch_size
+	ranges = [(i*batch_size, (i+1)*batch_size) for i in range(num_groups)]
+	ranges.append((num_groups * batch_size, len(iterable)))
+
+	result = [iterable.iloc[start:end] for start, end in ranges]
+
+	return result
+
+
+
 print(f'Duplicated Count: {accnos.duplicated().sum()}')
 accnos = accnos.drop_duplicates()
 print(f'Final Count: {len(accnos)}')
@@ -17,7 +28,15 @@ print(f'Final Count: {len(accnos)}')
 Entrez.email = 'john.palmer1288@gmail.com'
 Entrez.api_key = os.environ['NCBI_API_KEY']
 
-result = Entrez.efetch(db='nucleotide', id=accnos, rettype='fasta',retmode='text')
-with open(sys.argv[2], 'w') as outfile:
-	for i in result:
-		outfile.write(i)
+if len(accnos) <= 5000:
+	result = Entrez.efetch(db='nucleotide', id=accnos, rettype='fasta',retmode='text')
+	with open(sys.argv[2], 'w') as outfile:
+		for i in result:
+			outfile.write(i)
+else:
+	with open(sys.argv[2], 'w') as outfile:
+		for n, chunk in enumerate(split_groups(accnos)):
+			print(f"Processing chunk {n}")
+			result = Entrez.efetch(db='nucleotide', id=chunk.to_list(), rettype='fasta',retmode='text')
+			for i in result:
+				outfile.write(i)
